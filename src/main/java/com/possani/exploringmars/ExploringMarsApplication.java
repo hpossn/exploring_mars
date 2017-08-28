c85b678cdcfc4d9f0f1566343604d39f3fd04581
possani <hugopossani@gmail.com>
2017-08-27 21:14:26 -0300

    Closes #1 - Initial setup

---
+++ src/main/java/com/possani/exploringmars/ExploringMarsApplication.java
@@ -0,0 +1,12 @@
+package com.possani.exploringmars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringMarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploringMarsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/possani/exploringmars/ExploringMarsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.possani.exploringmars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExploringMarsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
